212d8320883ce0bbbb86e1d7f3789fc576634dba
NeanSword <69351960+NeanSword@users.noreply.github.com>
2026-09-25 23:31:37 +0200

    recomp: register generated NP3F overlays

---
+++ cmake/np3f_link_smoke/np3f_register_overlays.cpp
@@ -0,0 +1,21 @@
+#include "librecomp/overlays.hpp"
+#include "recomp_overlays.inl"
+
+namespace aerostadium2 {
+
+void register_np3f_overlays() {
+    recomp::overlays::overlay_section_table_data_t sections {
+        .code_sections = section_table,
+        .num_code_sections = ARRLEN(section_table),
+        .total_num_sections = num_sections,
+    };
+
+    recomp::overlays::overlays_by_index_t overlays {
+        .table = overlay_sections_by_index,
+        .len = ARRLEN(overlay_sections_by_index),
+    };
+
+    recomp::overlays::register_overlays(sections, overlays);
+}
+
+} // namespace aerostadium2
